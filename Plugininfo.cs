380d3f45dd4e0e753319777ba1ee8ff25382a685
TheRealBeef <84393940+TheRealBeef@users.noreply.github.com>
2025-09-24 09:38:01 +0200

    Version 2.0.1

--- Plugininfo.cs
+++ Plugininfo.cs
@@ -4,6 +4,6 @@ namespace BeefsRoverFixes
     {
         public const string PLUGIN_GUID = "BeefsRoverFixes";
         public const string PLUGIN_NAME = "Beef's Rover Fixes";
-        public const string PLUGIN_VERSION = "2.0.0";
+        public const string PLUGIN_VERSION = "2.0.1";
     }
 }
